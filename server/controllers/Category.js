d90c231617da52d2b18e8ae5afc0401642859354
SAH1L19 <asphaltking30@gmail.com>
2024-03-12 22:48:22 +0530

    ‎

---
+++ server/controllers/Category.js
@@ -0,0 +1,83 @@
+const Category = require("../models/Category");
+
+exports.createCategory = async(req,res)=>{
+    try{
+        const {name,description } =req.body;
+        if(!name){
+            return res.status(400).json({
+                success:false,
+                message:'All fields are required',
+            });
+        }
+        //create entry in db
+        const CategoryDetails = await Category.create({
+            name:name,description:description,
+        });
+        console.log(CategoryDetails);
+        return res.status(200).json({
+            success:true,
+            message:"Category created successfully",
+        });
+    }
+    catch(error){
+    
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+}
+
+exports.showAllCategories = async(req,res)=>{
+    try{
+      const allCategories = await Category.find({},{name:true,description:true});
+      return res.status(200).json({
+        success:true,
+        message:"All Categories obtained successfully",
+        allCategories,
+      })
+    } catch(err){
+      return res.status(500).json({
+        success:false,
+        message:err.message,
+      });
+    }
+  }
+  
+  exports.categoryDetails = async(req,res)=>{
+    try{
+        //course id 
+        const {categoryId}  =req.body;
+        //get courses for specified category id 
+        const selectedCategory = await Category.findById(categoryId).populate("courses").exec();
+        //validation
+        if(!selectedCategory){
+            return res.status(404).json({
+                success:false,
+                message:"Data not found",
+            });
+        }
+        //get category for different categories
+        const differentCategories= await Category.find({
+            _id:{$ne:categoryId}
+            //not equal to = $ne
+        }).populate("courses").exec();
+        
+        //get top 10  selling courses  //hwwww
+
+        //return response
+        return res.status(200).json({
+            success:true,
+            differentCategories,
+        });
+    }
+    catch(error){
+
+    return res.status(500).json({
+      success:false,
+      message:err.message,
+    });
+ 
+
+    }
+  }
--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -6,22 +6,129 @@ const Course = require("../models/Course");
 exports.createRating  = async(req,res)=>{
     try{
         //get user id 
+        const userId = req.user.id;
         //get course id 
-        //check if user is enrolled or not (only 1 time review and rating) 
+        const {rating,review,courseId} = req.body;
+        //check if user is enrolled or not (only 1 time review and rating)
+        const courseDetails = await Course.findone(
+            {_id:courseId,
+                studentsEnrolled:{$elemMatch:{$eq:userId}},
+            //fetches student from studentenrolled from course id       
+            });
         //create rationg and review 
-        //update course
+        if(!courseDetails){
+            return res.json({
+                success:false,
+                message:"Student is not enrolled in the course",
+            });
+        }
+        console.log(courseDetails);
+        //check if already reviewd or not 
+        const alreadyReviewed = await RatingAndReview.findOne({
+            user:userId,
+            course:courseId,          
+        });
+        if(alreadyReviewed){
+            return res.json({
+                success:false,
+                message:"Course is already reviewed by the user",
+            });
+        }
+        //create reivew and update course
+        const ratingReview = await RatingAndReview.create({
+            rating,review,course:courseId,user:userId,
+        });
+        await Course.findByIdAndUpdate(courseId,{
+            $push:{ratingAndReviews:ratingReview},
+            
+        },{new:true},
+        );
         //return response 
+        return res.status(200).json({
+            success:true,
+            message:"Ratind and review created successfully",
+        });
         
     }
     catch(error){
-
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
     }
 }
 
 //create average rating
+exports.getAverageRating = async(req,res)=>{
+    try{
+        //get courseId 
+        const courseId = req.body.courseId;
+        //calculate average rating 
+        const result = await RatingAndReview.aggregate(
+            [
+                {
+                    $match:{
+                        course:new mongoose.Types.ObjectId(courseId),
+                    }
+                },
+                {
+                    $group:{
+                        _id:null,
+                        averageRating:{$avg:"$rating"},
+                    }
+                }
+            ]
+        )
+        //return rating 
+        if(result.length>0){
+            return res.status(200).json({
+                succes:true,
+                averageRating:result[0].averageRating,
+            })
+        }
 
+        //if no rating review exist
+        return res.status(200).json({
+            success:true,
+            message:"Average Rating is 0 , no ratings given",
+            averageRating:0,
+        });
+        
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+}
 
 
 
 
 //get all ratings 
+
+exports.getAllRating = async(req,res)=>{
+    try{
+        const allReviews = await RatingAndReview.find(
+            {}).sort({rating:"desc"})
+            .populate({path:"user",select:"firstName lastName email image" })
+            .populate({path:"course",select:"courseName"})
+            .exec();
+
+
+        return res.status(200).json({
+            success:true,
+            message:"All reviews fetched successfully",
+            data:allReviews,
+        });
+    }
+    catch(error){
+        
+            return res.status(500).json({
+                success:false,
+                message:error.message,
+            });
+        
+    }
+}
